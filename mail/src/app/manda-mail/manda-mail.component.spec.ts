0c3f0a8a7ca4369fcc0d0cdf7f8c003ec6390105
Arba Marco <arba.marco@iisgalvanimi.edu.it>
2024-04-03 09:00:37 +0000

    primo commit

---
+++ mail/src/app/manda-mail/manda-mail.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MandaMailComponent } from './manda-mail.component';
+
+describe('MandaMailComponent', () => {
+  let component: MandaMailComponent;
+  let fixture: ComponentFixture<MandaMailComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [MandaMailComponent]
+    });
+    fixture = TestBed.createComponent(MandaMailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ mail/src/app/manda-mail/manda-mail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manda-mail',
+  templateUrl: './manda-mail.component.html',
+  styleUrls: ['./manda-mail.component.css']
+})
+export class MandaMailComponent {
+
+}
